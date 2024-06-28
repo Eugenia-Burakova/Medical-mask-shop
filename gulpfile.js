0feb2944aa3475df8afed8ed64b5e79bad9256ba
Eugenia Burakova <evzickabur@gmail.com>
2024-06-28 23:08:23 +0300

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest } =require("gulp");
+
+const sass = require('gulp-sass')(require('sass'));
+
+function generateCSS(cb) {
+    src('scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('css/'));
+    cb();
+}
+
+exports.css = generateCSS;
